7e5ece2a6230ffec30bf32480ea1e494015a94e4
Sarika <sarika.goooogle.1330@gmail.com>
2024-05-02 00:53:22 +0530

    done all

--- public/browser-app.js
+++ public/browser-app.js
@@ -9,12 +9,13 @@ async function showTasks() {
   try {
     let data = await fetch("/api/v1/tasks").then((res) => res.json());
     console.log(data);
-    if(data.length < 1) {
+    if (data.length < 1) {
       tasksDOM.innerHTML = '<h5 class="empty-list">No tasks in your list</h5>';
       loadingDOM.style.visibility = "hidden";
       return;
-    } 
-    const all=data.map((task) => {
+    }
+    const all = data
+      .map((task) => {
         const { completed, _id: taskID, name } = task;
         return `<div class="single-task ${completed && "task-completed"}">
                     <div class="headNo"> <h5>    <span>o  </span>${name} </h5></div>
@@ -30,30 +31,60 @@ async function showTasks() {
                     
                     </div>
                 </div>`;
-      }).join('')
-      tasksDOM.innerHTML = all
-    
+      })
+      .join("");
+    tasksDOM.innerHTML = all;
   } catch (error) {
     tasksDOM.innerHTML =
-    '<h5 class="empty-list">There was an error, please try later....</h5>'
-}
-loadingDOM.style.visibility = 'hidden'
-
+      '<h5 class="empty-list">There was an error, please try later....</h5>';
+  }
+  loadingDOM.style.visibility = "hidden";
 }
 
 showTasks();
 
-
 // delete task =>   functionality /api/tasks/:id
 
-async function deleteTask(id){
-    console.log(id)
-      try {
-        await fetch(`/api/v1/tasks/${id}`, { method: 'DELETE' });
-        showTasks()
-      } catch (error) {
-        console.log(error)
-      }
-    
-    loadingDOM.style.visibility = 'hidden'
+async function deleteTask(id) {
+  console.log(id);
+  try {
+    await fetch(`/api/v1/tasks/${id}`, { method: "DELETE" });
+    showTasks();
+  } catch (error) {
+    console.log(error);
   }
+
+  loadingDOM.style.visibility = "hidden";
+}
+
+formDOM.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const name = taskInputDOM.value;
+  const res = {
+    name: name,
+    completed: false,
+  };
+  try {
+    await fetch(`/api/v1/tasks`, {
+      method: "POST",
+      body: JSON.stringify(res),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }).then((response) => response.json());
+    // console.log(data);
+    
+    taskInputDOM.value = "";
+    formAlertDOM.style.display = "block";
+    formAlertDOM.textContent = `success, task added`;
+    formAlertDOM.classList.add("text-success");
+    showTasks();
+  } catch (error) {
+    formAlertDOM.style.display = "block";
+    formAlertDOM.innerHTML = `error, please try again`;
+  }
+  setTimeout(() => {
+    formAlertDOM.style.display = "none";
+    formAlertDOM.classList.remove("text-success");
+  }, 3000);
+});
